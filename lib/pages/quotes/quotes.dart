1f08eb856992f5d4379a5c1f4ef03bda6844cd15
Rishabh <44425012+ioxvar@users.noreply.github.com>
2022-05-26 01:50:26 +0530

    min

--- lib/pages/quotes/quotes.dart
+++ lib/pages/quotes/quotes.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:myboardapp/pages/quotes/constants.dart';
 import 'package:myboardapp/pages/quotes/quotes_list.dart';
 
@@ -13,43 +16,48 @@ class Quotes extends StatefulWidget {
 class _QuotesState extends State<Quotes> {
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
+    
     return Scaffold(
-      // ignore: prefer_const_constructors
+      
       body: Container(
-        color: Color.fromARGB(255, 10, 75, 107),
+        color: Color.fromARGB(255, 5, 33, 47),
         height: double.infinity,
         width: double.infinity,
-        child: CarouselSlider.builder(
-          itemCount: quotesList.length,
-          itemBuilder: (context, index1, index2) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Text(
-                    quotesList[index1][kQuote],
-                    style: quoteTextStyle,
+        child: SafeArea(
+          child: CarouselSlider.builder(
+            itemCount: quotesList.length,
+            itemBuilder: (context, index1, index2) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Text(
+                      quotesList[index1][kQuote],
+                      style: quoteTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '- ${quotesList[index1][kAuthor]} -',
+                    style: authorTextStyle,
                     textAlign: TextAlign.center,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  quotesList[index1][kAuthor],
-                  style: authorTextStyle,
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            );
-          },
-          options: CarouselOptions(
-            scrollDirection: Axis.vertical,
-            pageSnapping: true,
-            initialPage: 0,
-            enlargeCenterPage: true,
+                ],
+              );
+            },
+            options: CarouselOptions(
+              scrollDirection: Axis.vertical,
+              pageSnapping: true,
+              initialPage: 0,
+              enlargeCenterPage: true,
+              // onPageChanged: (index, value) {
+              //   HapticFeedback.lightImpact();
+              // },
+            ),
           ),
         ),
       ),
